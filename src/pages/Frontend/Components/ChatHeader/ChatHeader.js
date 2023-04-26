292735de3dd04296308eb0a77033ca3447dab14d
Muhammad Ahmed <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2023-04-26 17:41:18 +0100

    add profile img

--- src/pages/Frontend/Components/ChatHeader/ChatHeader.js
+++ src/pages/Frontend/Components/ChatHeader/ChatHeader.js
@@ -8,7 +8,7 @@ export default function ChatHeader() {
     <div className='Navbar'>
       <h6 className='fw-bold text-white mt-1'>Talk Now</h6>
       <div className="User">
-            <img src={profile} alt="profile" style={{width: 30}} className='img-fluid  rounded-circle' />
+            <img src={profile} alt="profile" style={{width: 30}} className='img-fluid  rounded-circle'/>
           <span className='fw-bold text-white mx-3'>john</span>
           <button className='btn btn-light btn-sm fw-bold btn-logout'>Logout</button>
       </div>
